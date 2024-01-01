2c1b1d3cb816d22322095ac80628481af84a9ab4
rizvan <rizvangaripov@gmail.com>
2024-01-01 22:27:03 +0300

    add some code

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ is_connected = False
 
 
 async def send_icmp(target_ip, data_to_send):
-    if (data_to_send != ""):
+    if data_to_send.strip() != "":
         packet = IP(dst=target_ip) / ICMP(id=1515) / data_to_send
         send(packet, verbose=False)
     # x = Ether(src='f0:d4:15:84:6b:65', dst='08:00:27:0f:73:c0')
