13650ef786ad751f0f09b56f9723aca139af681d
rizvan <rizvangaripov@gmail.com>
2024-01-02 11:58:14 +0300

    add some code

--- asd.py
+++ asd.py
@@ -1,9 +1,19 @@
 import subprocess
 
 if __name__ == "__main__":
-    command = "ls -l"
-    result = subprocess.run(command, shell=True, check=True, stdout=subprocess.PIPE, text=True)
+    command = "bash"
+    process = subprocess.Popen(command, stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.PIPE,
+                               shell=True)
 
-    # Получить вывод команды
-    output = result.stdout
-    print(output)
+    # Пример отправки команды
+    command_to_send = "ls -l"
+    process.stdin.write(command_to_send.encode('utf-8'))
+    process.stdin.write(b'\n')  # Добавляем новую строку, если нужно
+
+    # Получить вывод
+    output, errors = process.communicate()
+
+    # Декодируем вывод в строку
+    output_str = output.decode('utf-8')
+
+    print(output_str)
