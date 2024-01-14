0cbf7b93e9b4acf475175ff9d9bb80dc96fdf8d4
Rizvan <rizvangaripov@gmail.com>
2024-01-14 12:04:38 -0800

    fix bugs

--- client.py
+++ client.py
@@ -70,11 +70,7 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     if args.i:
-        i = int(args.i)
-        if 0 > i > 65535:
-            print("Идентификатор должен быть от 0 до 65535")
-            exit()
-        config.ID = i
+        config.ID = int(args.i)
     if args.t:
         config.TYPE = int(args.t)
     host = args.host
